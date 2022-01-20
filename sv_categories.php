52a248c5c1236ab61616c56cc8f13e67dbb702b8
Matthias Bathke <matthias-bathke@straightvisions.com>
2022-01-20 18:41:50 +0100

    FEAT - ADD - CATEGORY FEATURED IMAGE FIX - LOAD CATEGORY MODULE ONLY WHEN SV100 IS ACTIVE

--- sv_categories.php
+++ sv_categories.php
@@ -1,32 +1,37 @@
 <?php
 	namespace sv100_companion;
-	
-	/**
-	 * @version         4.000
-	 * @author			straightvisions GmbH
-	 * @package			sv100
-	 * @copyright		2019 straightvisions GmbH
-	 * @link			https://straightvisions.com
-	 * @since			1.000
-	 * @license			See license.txt or https://straightvisions.com
-	 */
-	
+
 	class sv_categories extends modules {
 		public function init() {
-			add_action('init', function(){
-				// filter name: sv100_companion_sv_categories_custom_fields
-				foreach(apply_filters($this->get_prefix('custom_fields'), array('category')) as $taxonomy){
-					add_action( $taxonomy.'_edit_form_fields', array( $this, 'edit_category_form_fields' ) );
-					add_action( $taxonomy.'_add_form_fields', array( $this, 'edit_category_form_fields' ) );
-					add_action( 'edited_'.$taxonomy, array( $this, 'edited_category' ) );
-				}
-			});
+			add_action('init', array($this, 'load'), 5);
+		}
+		public function load(){
+			if($this->get_instance('sv100')) {
+				add_action('init', function () {
+					// filter name: sv100_companion_sv_categories_custom_fields
+					foreach (apply_filters($this->get_prefix('custom_fields'), array('category')) as $taxonomy) {
+						add_action($taxonomy . '_edit_form_fields', array($this, 'edit_category_form_fields'));
+						add_action($taxonomy . '_add_form_fields', array($this, 'edit_category_form_fields'));
+						add_action('edited_' . $taxonomy, array($this, 'edited_category'));
+					}
+				});
 
-			if(!is_admin()){
-				add_action( 'pre_get_posts', array($this, 'pre_get_posts') );
+				if (!is_admin()) {
+					add_action('pre_get_posts', array($this, 'pre_get_posts'));
+				} else {
+					add_action('admin_enqueue_scripts', array($this, 'media_upload'));
+				}
 			}
 		}
-
+		public function media_upload(){
+			if(get_current_screen() && get_current_screen()->id === 'edit-category'){
+				$this->get_script('media_upload')
+					->set_type('js')
+					->set_path('lib/js/backend/media_upload.js')
+					->set_is_backend()
+					->set_is_enqueued();
+			}
+		}
 		public function pre_get_posts($query){
 			$order_by		= get_term_meta(get_queried_object_id(), 'sv100_companion_sv_categories_order_by', true);
 			$order			= get_term_meta(get_queried_object_id(), 'sv100_companion_sv_categories_order', true);
@@ -48,6 +53,7 @@
 			$order				= get_term_meta( $term->term_id, $this->get_prefix( 'order' ), true );
 			$template_style		= get_term_meta( $term->term_id, $this->get_prefix( 'template_style' ), true );
 			$page				= get_term_meta( $term->term_id, $this->get_prefix( 'page' ), true );
+			$featured_image		= get_term_meta( $term->term_id, $this->get_prefix( 'featured_image' ), true );
 			?>
 			<tr class="form-field term-<?php echo $this->get_prefix( 'order_by' ); ?>-wrap">
 				<th scope="row"><label for="<?php echo $this->get_prefix( 'order_by' ); ?>"><?php _e( 'Order by', 'sv100_companion' ); ?></label></th>
@@ -101,6 +107,22 @@
 					</select>
 				</td>
 			</tr>
+			<tr class="form-field term-<?php echo $this->get_prefix( 'featured_image' ); ?>-wrap">
+				<th scope="row"><label for="<?php echo $this->get_prefix( 'featured_image' ); ?>"><?php _e( 'Category Featured Image', 'sv100_companion' ); ?></label></th>
+				<td>
+					<input type="hidden" id="<?php echo $this->get_prefix( 'featured_image' ); ?>" name="<?php echo $this->get_prefix( 'featured_image' ); ?>" class="custom_media_url" value="<?php echo $featured_image; ?>">
+					<div id="<?php echo $this->get_prefix( 'featured_image_wrapper' ); ?>">
+						<?php if ( $featured_image ) { ?>
+							<?php echo wp_get_attachment_image ( $featured_image, 'thumbnail' ); ?>
+						<?php } ?>
+					</div>
+					<p>
+						<input type="button" class="button button-secondary ct_tax_media_button" id="ct_tax_media_button" name="ct_tax_media_button" value="<?php _e( 'Add Image', 'sv100_companion' ); ?>" />
+						<input type="button" class="button button-secondary ct_tax_media_remove" id="ct_tax_media_remove" name="ct_tax_media_remove" value="<?php _e( 'Remove Image', 'sv100_companion' ); ?>" />
+					</p>
+					<p class="description">Select Featured Image for Category</p>
+				</td>
+			</tr>
 			<!--
 			<tr class="form-field term-<?php echo $this->get_prefix( 'page' ); ?>-wrap">
 				<th scope="row"><label for="<?php echo $this->get_prefix( 'page' ); ?>"><?php _e( 'Page', 'sv100_companion' ); ?></label></th>
@@ -130,6 +152,9 @@
 
 			if ( isset( $_POST[ $this->get_prefix( 'template_style' ) ] ) ) {
 				update_term_meta( $_POST['tag_ID'], $this->get_prefix( 'template_style' ), $_POST[ $this->get_prefix( 'template_style' ) ] );
+			}
+			if( isset( $_POST[$this->get_prefix( 'featured_image' )] ) && '' !== $_POST[$this->get_prefix( 'featured_image' )] ){
+				update_term_meta( $_POST['tag_ID'], $this->get_prefix( 'featured_image' ), $_POST[$this->get_prefix( 'featured_image' )] );
 			}
 /*
 			if ( isset( $_POST[ $this->get_prefix( 'page' ) ] ) ) {
